3fa859c14fae6b17a9f0df06ccfbbcbc24588a41
EGERMAN <etgermanovich@gmail.com>
2022-12-13 21:57:11 -0500

    reworked BootStrapData for correct order of storing data after creation

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -27,40 +27,48 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Starting in Bootstrap");
 
+        //Create publisher
         Publisher superBooksPublishing = new Publisher(
                 "Super Books Publishing",
                 "101 Fire St.",
-                "Virignia Beach",
+                "Virginia Beach",
                 "VA",
                 23451
         );
 
+        //save newly created publisher to DB
         publisherRepository.save(superBooksPublishing);
+
         System.out.println("Publisher Count: " + publisherRepository.count());
 
-
+        //create new author and book, add them to each other
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        //add publisher to book ddd and vice versa
+        ddd.setPublisher(superBooksPublishing);
+        superBooksPublishing.getBooks().add(ddd);
+
+        //save new author, book, and publisher to DB
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(superBooksPublishing);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
-
-        ddd.setPublisher(superBooksPublishing);
-        superBooksPublishing.getBooks().add(ddd);
-        publisherRepository.save(superBooksPublishing);
-
         noEJB.setPublisher(superBooksPublishing);
         superBooksPublishing.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(superBooksPublishing);
+
+
 
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Publisher Number of Books: " + superBooksPublishing.getBooks().size());
---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    //Because we have a constructor here, and this is a Spring managed component because it is a controller,
+    //when Spring instantiates this, it will inject an instance of that book repository into our controller.
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books/list";
+    }
+}
